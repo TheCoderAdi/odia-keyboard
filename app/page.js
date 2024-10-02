47e2719e8980fb559ec3b8ebaaae512406c46a3d
Aditya <adityasahoo652@gmail.com>
2024-10-02 23:02:54 +0530

    chore: Refactor OdiaKeyboard component and add copy to clipboard functionality

--- app/page.js
+++ app/page.js
@@ -1,101 +1,75 @@
-import Image from "next/image";
+"use client";
+import { useRef, useState } from "react";
+import { vowels, consonants, matras, conjuncts, boxArray } from "@/utils/utils";
+import Box from "@/components/Box";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function OdiaKeyboard() {
+  const [text, setText] = useState("");
+  const textAreaRef = useRef(null);
+  const { toast } = useToast();
+
+  const handleLetterClick = (letter) => {
+    const textArea = textAreaRef.current;
+    const start = textArea.selectionStart;
+    const end = textArea.selectionEnd;
+
+    const updatedText = text.slice(0, start) + letter + text.slice(end);
+    setText(updatedText);
+
+    if (window.innerWidth < 768) {
+      textArea.focus();
+    }
+
+    setTimeout(() => {
+      textArea.selectionStart = textArea.selectionEnd = start + letter.length;
+    }, 0);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(text);
+    toast({
+      title: "Copied to clipboard",
+      description: "The text has been copied to your clipboard",
+    });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-8 w-full">
+      <h1 className="text-4xl font-bold mb-8 text-center max-md:text-2xl max-md:mb-4">Odia Keyboard</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="flex w-full max-w-6xl space-x-8 max-md:flex-col max-md:justify-center max-md:items-center">
+        <div className="flex flex-col w-1/2 sticky max-md:relative top-8 max-md:w-full max-md:mb-20">
+          <textarea
+            value={text}
+            ref={textAreaRef}
+            onChange={(e) => setText(e.target.value)}
+            rows="10"
+            className="w-full p-4 mb-6 bg-gray-900 text-white border border-gray-700 rounded-lg focus:outline-none text-2xl focus:ring-2 focus:ring-indigo-500"
+            placeholder="Type here or use the keyboard below..."
+          />
+          <Button
+            onClick={copyToClipboard}
+            variant="outline"
+            className="w-[30%] max-md:w-full text-black p-6 font-bold"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Copy to clipboard
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex flex-col w-1/2 space-y-6 max-h-[500px] overflow-y-auto max-md:w-full">
+          {boxArray.map((box, index) => (
+            <Box
+              key={`index-${index}-${box.title}`}
+              title={box.title}
+              letters={box.letters}
+              handleLetterClick={handleLetterClick}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+
