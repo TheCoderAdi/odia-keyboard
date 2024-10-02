22bfcb45deb5eb54945c86e314ce7173dd8e9eeb
Aditya <adityasahoo652@gmail.com>
2024-10-02 23:03:25 +0530

    feat: Add Box component for displaying letters

---
+++ components/Box.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Box = ({ title, letters, handleLetterClick }) => {
+  return (
+    <div className="mb-5 w-full max-w-2xl">
+      <h3 className="text-xl font-semibold mb-4">{title}</h3>
+      <div className="flex flex-wrap justify-center space-x-2 space-y-2 max-md:justify-start">
+        {letters.map((letter) => (
+          <button
+            key={letter}
+            onClick={() => handleLetterClick(letter)}
+            className="bg-white hover:bg-gray-300 text-black font-semibold py-2 px-4 rounded-lg shadow-lg transition-transform transform hover:scale-105 text-2xl max-md:text-xl"
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Box;
