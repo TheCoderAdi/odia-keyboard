a2e28870847e9c91e9514a17ab47cafd4f2f55f5
Aditya <adityasahoo652@gmail.com>
2024-10-09 10:11:46 +0530

    feat: Update intro.js tutorial steps

---
+++ utils/steps.js
@@ -0,0 +1,54 @@
+export const steps = [
+    {
+        element: ".tut-theme-change",
+        intro: "This is your theme toggle button!",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".tut-textarea",
+        intro: "This is your text area where you can type or paste text.",
+        position: "bottom",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".tut-copy-button",
+        intro: "Click here to copy the text to your clipboard",
+        position: "left",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".box-0",
+        intro: "These are the vowels. Click on them to type them in the text area.",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".box-1",
+        intro:
+            "These are the consonants. Click on them to type them in the text area.",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".box-2",
+        intro:
+            "These are the special Matras. Click on them to type them in the text area.",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".box-3",
+        intro:
+            "These are the conjucts. Click on them to type them in the text area.",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+    {
+        element: ".box-4",
+        intro:
+            "These are the numbers. Click on them to type them in the text area.",
+        position: "right",
+        tooltipClass: "dark:bg-black dark:text-white",
+    },
+];
